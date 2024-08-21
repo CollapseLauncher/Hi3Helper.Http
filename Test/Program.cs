22c22981032cbe410950c67e0bfe03c7d0e9d7f6
Kemal Setya Adhi <dev.kemalsetyaa@gmail.com>
2024-08-21 23:51:49 +0700

    Update test

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,121 +1,92 @@
 ﻿using Hi3Helper.Http;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.IO.Compression;
 using System.Net.Http;
-using System.Threading;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Test
 {
+    [JsonSerializable(typeof(PkgVersion))]
+    public partial class JsonContext : JsonSerializerContext { }
+
+    public class PkgVersion
+    {
+        public required string remoteName { get; set; }
+        public required string md5 { get; set; }
+        public long fileSize { get; set; }
+    }
+
     internal class Program
     {
-        static string URL = "https://autopatchos.zenlesszonezero.com/pclauncher/nap_global/audio_ja-jp_1.0.1_1.1.0_hdiff_icFimTTzuCqPhGgs.zip";
-        static string Output = @".\Testing.diff";
-        static string Output2 = @"C:\Users\neon-nyan\AppData\LocalLow\CollapseLauncher\GameFolder\Hi3TW\Games\BH3_v5.9.0_cba771e4ca76.7z.001";
-        static string Output3 = @"C:\Users\neon-nyan\AppData\LocalLow\CollapseLauncher\GameFolder\Hi3TW\Games\BH3_v5.9.0_cba771e4ca76.7z.dummy";
-        static string Output4 = @"C:\Users\neon-nyan\Downloads\bin\YuanShen_2.8.54_beta.zip";
-
         static async Task Main()
         {
-            #region testing
-            // Console.WriteLine("Downloading sample...");
-            // if (File.Exists(Output))
-            //     File.Delete(Output);
+            const string ExtractUrl = @"https://autopatchcn.bh3.com/ptpublic/rel/20240805104059_mJaMrTOz6gCYOVBW/PC/extract/";
+            const string PkgVersionUrl = ExtractUrl + "pkg_version";
+            const string OutputPath = @"E:\myGit\CollapseLauncher-ReleaseRepo\diff\test";
 
-            // Output = Output4;
-            // await Client.Download(URL, Output);
-            /*
-
-            Stopwatch sw = Stopwatch.StartNew();
-
-            int buffLength = 4 << 20;
-            using (FileStream fs = new FileStream(Output, FileMode.Open, FileAccess.Read))
-            {
-                int read;
-                byte[] buf = new byte[buffLength];
-                while ((read = fs.Read(buf, 0, buf.Length)) > 0)
-                {
-                    sh64.ComputeHash32(buf, read);
-                }
-            }
-
-            Console.WriteLine("SimpleChecksum (Int32/int mode): {0:00}:{1:00}:{2:00}.{3:00}", sw.Elapsed.Hours, sw.Elapsed.Minutes, sw.Elapsed.Seconds, sw.Elapsed.Milliseconds / 10);
-            Console.WriteLine("Hash: {0}", sh64.HashString32);
-
-            sw = Stopwatch.StartNew();
-
-            sh64.Flush();
-            using (FileStream fs = new FileStream(Output, FileMode.Open, FileAccess.Read))
-            {
-                int read;
-                byte[] buf = new byte[buffLength];
-                while ((read = fs.Read(buf, 0, buf.Length)) > 0)
-                {
-                    sh64.ComputeHash64(buf, read);
-                }
-            }
-
-            Console.WriteLine("SimpleChecksum (Int64/long mode): {0:00}:{1:00}:{2:00}.{3:00}", sw.Elapsed.Hours, sw.Elapsed.Minutes, sw.Elapsed.Seconds, sw.Elapsed.Milliseconds / 10);
-            Console.WriteLine("Hash: {0}", sh64.HashString64);
-
-            sw = Stopwatch.StartNew();
-
-            crc = new();
-            using (FileStream fs = new FileStream(Output, FileMode.Open, FileAccess.Read))
-            {
-                int read;
-                byte[] buf = new byte[buffLength];
-                while ((read = fs.Read(buf, 0, buf.Length)) > 0)
-                {
-                    crc.TransformBlock(buf, 0, read, buf, 0);
-                }
-                crc.TransformFinalBlock(buf, 0, 0);
-            }
-
-            Console.WriteLine("Force's CRC32: {0:00}:{1:00}:{2:00}.{3:00}", sw.Elapsed.Hours, sw.Elapsed.Minutes, sw.Elapsed.Seconds, sw.Elapsed.Milliseconds / 10);
-            Console.WriteLine("Hash: {0}", BytesToHex(crc.Hash));
-            */
-            #endregion
-            /*
-            long limit = 1200000000;
-            byte[] buffer = new byte[4096];
-            SimpleChecksum partialHash = new();
-
-            using (Stream fs = new FileStream(Output4, FileMode.Open, FileAccess.Read))
-            {
-                int Read;
-                int NextRead = buffer.Length;
-                while ((Read = fs.Read(buffer, 0, NextRead)) > 0)
-                {
-                    NextRead = (int)Math.Min(buffer.Length, limit - fs.Position);
-                    partialHash.ComputeHash32(buffer, Read);
-                }
-            }
-            */
-            while (true)
             {
                 try
                 {
-                    using (HttpClientHandler handler = new HttpClientHandler())
+                    using (HttpClientHandler handler = new HttpClientHandler() { MaxConnectionsPerServer = 2048 })
                     using (HttpClient client = new HttpClient(handler, false))
-                    using (MemoryStream stream = new MemoryStream())
-                    using (BrotliStream encStream = new BrotliStream(stream, CompressionLevel.Fastest))
+                    using (HttpRequestMessage pkgVersionRequestMessage = new HttpRequestMessage()
                     {
-                        sw = Stopwatch.StartNew();
-                        lastDownloaded = 0;
+                        RequestUri = new Uri(PkgVersionUrl)
+                    })
+                    using (HttpResponseMessage pkgVersionResponseMessage = await client.SendAsync(pkgVersionRequestMessage))
+                    using (Stream pkgVersionStream = await pkgVersionResponseMessage.Content.ReadAsStreamAsync())
+                    {
                         DownloadClient downloader = DownloadClient.CreateInstance(client);
-                        DownloadClient.SetSharedDownloadSpeedLimit(1 << 20);
-                        // await downloader.DownloadAsync(URL, Output, false, null, null, DownloadProgressDelegateAsync, 4, 8 << 20);
+                        await Parallel.ForEachAsync(
+                            DeserializePkgVersion(pkgVersionStream),
+                            new ParallelOptions
+                            {
+                                MaxDegreeOfParallelism = Environment.ProcessorCount
+                            },
+                            async (pkgVersion, token) =>
+                        {
+                            string outputPath = Path.Combine(OutputPath, pkgVersion.remoteName);
+                            string outputDir = Path.GetDirectoryName(outputPath)!;
+                            string fileUrl = ExtractUrl + pkgVersion.remoteName;
 
-                        await downloader.DownloadAsync(URL, encStream, false, DownloadProgressDelegateAsync, null, null);
+                            if (!Directory.Exists(outputDir))
+                                Directory.CreateDirectory(outputDir);
+
+                            sw = Stopwatch.StartNew();
+                            lastDownloaded = 0;
+
+                            // DownloadClient.SetSharedDownloadSpeedLimit(1 << 20);
+                            Console.WriteLine("Downloading: {0}", pkgVersion.remoteName);
+                            await downloader.DownloadAsync(fileUrl, outputPath, true, null, null, DownloadProgressDelegateAsync, Environment.ProcessorCount);
+
+                            byte[] remoteHash = Convert.FromHexString(pkgVersion.md5);
+
+                            using FileStream fileStream = File.OpenRead(outputPath);
+                            byte[] currentHash = await MD5.HashDataAsync(fileStream);
+
+                            if (!currentHash.AsSpan().SequenceEqual(remoteHash))
+                                throw new Exception();
+                        });
                     }
                 }
                 catch (OperationCanceledException)
                 {
                     Console.WriteLine($"Cancelled!");
                 }
+            }
+        }
+
+        private static async IAsyncEnumerable<PkgVersion> DeserializePkgVersion(Stream stream)
+        {
+            StreamReader pkgVersionTextReader = new StreamReader(stream);
+            while (!pkgVersionTextReader.EndOfStream)
+            {
+                yield return JsonSerializer.Deserialize(await pkgVersionTextReader.ReadLineAsync() ?? "", JsonContext.Default.PkgVersion)!;
             }
         }
 
@@ -141,7 +112,7 @@ namespace Test
                 string.Format("{0:%h}h{0:%m}m{0:%s}s left", timeLeft)
                 );
 
-            int spaceToWrite = Math.Max(0, toWrite.Length - Console.CursorLeft - 1);
+            int spaceToWrite = Math.Max(0, Console.BufferWidth - 1 - toWrite.Length);
             toWrite += new string(' ', spaceToWrite) + '\r';
             Console.Write(toWrite);
         }
@@ -154,12 +125,5 @@ namespace Test
 
             return string.Format("{0} {1}", Math.Round(value / (1L << (mag * 10)), decimalPlaces), SizeSuffixes[mag]);
         }
-
-        private static async void WaitAndCancel()
-        {
-            await Task.Delay(250);
-        }
-
-        public static string BytesToHex(byte[] bytes) => BitConverter.ToString(bytes).Replace("-", string.Empty);
     }
 }
